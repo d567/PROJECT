e272175ad66b3c4d99c169ad996c679731400c67
d567 <47376782+d567@users.noreply.github.com>
2019-02-06 02:49:03 -0800

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		for(int i=0;i<a;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
