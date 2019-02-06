578aaa9d078a1ceaad4c4d0c3609f04a512f4e69
d567 <47376782+d567@users.noreply.github.com>
2019-02-06 00:05:12 -0800

    Update PosNeg.java

--- PosNeg.java
+++ PosNeg.java
@@ -1,1 +1,26 @@
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a;
+	Scanner in=new Scanner(System.in);
+	a=in.nextInt();
+	if(a>0)
+	{
+		System.out.println("Positive");
+	}
+	else if(a<0)
+	{
+		System.out.println("Negative");
+	}
+	else
+	{
+		System.out.println("Zero");
+	}
+	}
+}
