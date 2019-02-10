8b50d71c8f587d083d6205cb82e7d2c6d7dcbf27
d567 <47376782+d567@users.noreply.github.com>
2019-02-10 15:22:24 +0530

    Create SumOfN.java

---
+++ SumOfN.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int n,i,sum=0;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		for(i=1;i<=n;i++)
+		{
+		sum=sum+i;	
+		}
+		System.out.println(sum);
+	}
+}
