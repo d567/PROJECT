851e3c820839682e78253a9518b2b9fa9cb72cb2
d567 <47376782+d567@users.noreply.github.com>
2019-03-08 21:14:05 +0530

    Create multiplies.java

---
+++ multiplies.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class multiplies
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print(n*i+" ");
+		}
+	}
+}
