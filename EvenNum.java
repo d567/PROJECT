043dc5ce2d0e4d947c8d295d4cf77b5272229307
d567 <47376782+d567@users.noreply.github.com>
2019-03-08 20:00:40 +0530

    Create EvenNum.java

---
+++ EvenNum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class EvenNum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a,b,i;
+	Scanner in=new Scanner(System.in);
+	a=in.nextInt();
+	b=in.nextInt();
+	for(i=a+1;i<b;i++)
+	{
+		if(i%2==0)
+		{
+			System.out.print(i+" ");
+		}
+	}
+	}
+}
